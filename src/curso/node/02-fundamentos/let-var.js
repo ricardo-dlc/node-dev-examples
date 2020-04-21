d462e4c40e134fb9d761af9d86a8cdd5b0fede51
Ricardo de la Cruz <the_phantom_racer@hotmail.com>
2020-04-20 19:40:27 -0500

    02 Fundamentos
    - N: src/curso/node/02-fundamentos/
      - Ejercicios sobre fundamentos

---
+++ src/curso/node/02-fundamentos/let-var.js
@@ -0,0 +1,14 @@
+// let nombre = 'Wolverine';
+
+// if (true) {
+//     let nombre = 'Mageneto';
+// }
+
+// console.log(nombre);
+let i = 'Hola mundo';
+
+for (let i = 0; i <= 5; i++) {
+    console.log(`i: ${i}`);
+}
+
+console.log(i);
