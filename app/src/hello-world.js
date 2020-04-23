b5cc7a5eca1a9f0d69be3d60e3e7c8f486c61eee
Ricardo de la Cruz <the_phantom_racer@hotmail.com>
2020-04-22 22:40:08 -0500

    Reestructuración del proyecto para mejorar el desarrollo
    - M: Dockerfile
      - Se modifica la ruta de la instrucción COPY.
      - Se eliminan líneas innecesarias.
    
    - Se renombra la caarpeta src/ a app/
      - R: src/package-lock.json -> app/package-lock.json
      - R: src/package.json -> app/package.json
    
    - Se mueven los archivos de / a src/ utilizando el nuevo esquema
      - R: src/express.js -> app/src/express.js
      - R: src/hello-world.js -> app/src/hello-world.js
    
    - N: docker-compose.prod.yml
      - Archivo docker-compose para pruebas utilizando overriding.
    
    - N: Dockerfile.prod
      - Dockerfile correspondiente al archivo docker-compose.prod.yml.
    
    - R: docker-compose.dev.yml -> docker-compose.yml
      - Se renombra el archivo para ser utilizado como principal.

--- app/src/hello-world.js
+++ app/src/hello-world.js
@@ -1,12 +1,12 @@
 const http = require('http');
 
 const hostname = '0.0.0.0';
-const port = 8001;
+const port = 8002;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World! :3\n');
+  res.end('Hello, World! :$\n');
 });
 
 server.listen(port, hostname, () => {
